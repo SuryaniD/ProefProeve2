6b2bfd6e4caa1251f937b9bc627f8111bf13ce97
Suryani <bytebytermail@gmail.com>
2021-03-03 09:33:07 +0100

    First commit, added pedometer and made unity project

---
+++ StepCounter/Assets/Scripts/Pedometer/Pedometer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pedometer : MonoBehaviour
+{
+    public Text steps;
+
+    Gyroscope Gyro;
+
+    float stepDelta = 0.2f;
+    bool stepMade = false;
+    //Made public for integration with other scripts
+    public int amountOfSteps = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnableGyro();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSteps();
+    }
+
+    private void EnableGyro()
+    {
+        //Get gyro input and enable it
+        Gyro = Input.gyro;
+        Gyro.enabled = true;
+    }
+
+    private void UpdateSteps()
+    {
+        float movementDetection = Gyro.userAcceleration.z;
+
+        if (!stepMade)
+        {
+            if (movementDetection >= stepDelta)
+            { //Movement up? Start step
+                stepMade = true;
+            }
+        }
+
+        if (stepMade)
+        {
+            if (movementDetection <= -stepDelta)
+            { //Movement down? End step and count
+                stepMade = false;
+                amountOfSteps++;
+                steps.text = amountOfSteps.ToString();
+            }
+        }
+    }
+}
