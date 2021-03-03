303d3ef9c4d1e8bec100c81d7241a411ab9c978b
Toon <toonschaap3@gmail.com>
2021-03-03 15:48:21 +0100

    xp bar commit 1
    start of the script, comments still need to be added and still needs to be tested with phone

---
+++ StepCounter/Assets/Scripts/XP bar/XPbar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XPbar : MonoBehaviour
+{
+    private Slider expBar;
+    public float Progress;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        expBar = gameObject.GetComponent<Slider>();
+        expBar.value = Progress;
+        GameObject Importer = GameObject.Find("ScriptManager");
+        Pedometer pedometer = Importer.GetComponent<Pedometer>();
+
+       if (pedometer.amountOfSteps == pedometer.amountOfSteps++)
+        {
+            Progress += 1;
+        }
+    }
+
+}
