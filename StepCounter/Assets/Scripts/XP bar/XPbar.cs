e61a94432cd9a9646032a0dde3f52dbed3eda81d
Toon <toonschaap3@gmail.com>
2021-03-04 13:11:23 +0100

    XPbar push 3
    all previous bugs solved

--- StepCounter/Assets/Scripts/XP bar/XPbar.cs
+++ StepCounter/Assets/Scripts/XP bar/XPbar.cs
@@ -5,12 +5,10 @@ using UnityEngine.UI;
 
 public class XPbar : MonoBehaviour
 {
-    //slider itself
-    private Slider expBar;
 
-    //the number of steps taken put into the xp bar
-    public float Progress;
-    private int prevStepCount;
+    private float Progress;
+    private int stepPrev;
+    private Slider expBar;
 
     // Update is called once per frame
     void Update()
@@ -20,18 +18,11 @@ public class XPbar : MonoBehaviour
         GameObject Importer = GameObject.Find("ScriptManager");
         Pedometer pedometer = Importer.GetComponent<Pedometer>();
 
-        //if a value is added to amountOfSteps, 1 is added to Progress
-    
+        if (pedometer.amountOfSteps > stepPrev)
+       {
+            Progress += 1;
 
-        if (pedometer.amountOfSteps > prevStepCount) {​​​​​​​​
-        Progress += 1;
-
-        prevStepCount = pedometer.amountOfSteps;
-            }​​​​​​     ​​
-        }​​​​​​​​
-
-
+            stepPrev = pedometer.amountOfSteps;
         }
-    
-
-
+    }
+}
