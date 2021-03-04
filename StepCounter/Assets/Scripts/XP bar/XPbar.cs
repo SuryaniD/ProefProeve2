a6a335ee142e3a633a361047e1b98beb822a6e2b
Toon <toonschaap3@gmail.com>
2021-03-04 11:36:53 +0100

    XPbar push 2
    error CS1056: Unexpected character '?'
    caused after switching platform for building(?)

--- StepCounter/Assets/Scripts/XP bar/XPbar.cs
+++ StepCounter/Assets/Scripts/XP bar/XPbar.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class XPbar : MonoBehaviour
 {
+    //slider itself
     private Slider expBar;
+
+    //the number of steps taken put into the xp bar
     public float Progress;
-   
+    private int prevStepCount;
 
     // Update is called once per frame
     void Update()
@@ -17,10 +20,18 @@ public class XPbar : MonoBehaviour
         GameObject Importer = GameObject.Find("ScriptManager");
         Pedometer pedometer = Importer.GetComponent<Pedometer>();
 
-       if (pedometer.amountOfSteps == pedometer.amountOfSteps++)
-        {
-            Progress += 1;
-        }
-    }
+        //if a value is added to amountOfSteps, 1 is added to Progress
+    
 
-}
+        if (pedometer.amountOfSteps > prevStepCount) {​​​​​​​​
+        Progress += 1;
+
+        prevStepCount = pedometer.amountOfSteps;
+            }​​​​​​     ​​
+        }​​​​​​​​
+
+
+        }
+    
+
+
