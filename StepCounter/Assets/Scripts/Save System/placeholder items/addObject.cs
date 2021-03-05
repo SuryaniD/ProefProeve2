088afd2df866458b86cb9c5856635eb9f067e72d
Toon <toonschaap3@gmail.com>
2021-03-05 16:36:34 +0100

    save system push 1
    unity doesnt recognize Application.persistantDataPath

---
+++ StepCounter/Assets/Scripts/Save System/placeholder items/addObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class addObject : MonoBehaviour
+{
+    public GameObject spawn;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(spawn, new Vector2(0, 0), Quaternion.identity);
+
+        }
+    }
+}
---
+++ StepCounter/Assets/Scripts/Save System/saveManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class saveManager
+{
+    public static void save()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = new Application.persistentDataPath + "/player.save";
+
+    }
+}
