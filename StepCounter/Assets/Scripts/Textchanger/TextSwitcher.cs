e4201397f0838645b1c3a5c75fb93e79be6cb217
Suryani <bytebytermail@gmail.com>
2021-03-05 12:47:05 +0100

    Added text which shows text goal, still working on the text switching

---
+++ StepCounter/Assets/Scripts/Textchanger/TextSwitcher.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSwitcher : MonoBehaviour
+{
+    public Text talkText;
+
+    private int stepsLeft = 1000;
+    private int pedometerSteps;
+    public Pedometer pedometer;
+
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetPedoMeter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountstepsLeftDown();
+        ChangeText();
+    }
+
+    public void GetPedoMeter()
+    {
+        Pedometer pedometer = GameObject.Find("ScriptManager").GetComponent<Pedometer>();
+        
+    }
+
+    private void CountstepsLeftDown()
+    {
+        if(pedometer.amountOfSteps > pedometerSteps)
+        {
+            pedometerSteps = pedometer.amountOfSteps;
+            stepsLeft -= 1;
+        }
+    }
+
+    private void ChangeText()
+    {
+        talkText.text = "Nog " + stepsLeft + " stappen over";
+    }
+}
