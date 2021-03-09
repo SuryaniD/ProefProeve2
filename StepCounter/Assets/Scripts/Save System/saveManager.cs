86cf36d35a973c30622e7f2817a9ccba238ce27a
Toon <toonschaap3@gmail.com>
2021-03-09 12:57:14 +0100

    Save system push
    save system cancelled, focused on merging instead

--- StepCounter/Assets/Scripts/Save System/saveManager.cs
+++ StepCounter/Assets/Scripts/Save System/saveManager.cs
@@ -9,7 +9,32 @@ public static class saveManager
     public static void save()
     {
         BinaryFormatter formatter = new BinaryFormatter();
+
+        //creates path where the file is saved
         string path = new Application.persistentDataPath + "/player.save";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        TrophySystem data = new TrophySystem();
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static void load()
+    {
+        string path = new Application.persistentDataPath + "/player.save";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+           TrophySystem data = formatter.Deserialize(stream) as TrophySystem;
+            stream.Close();
+            return data;
+        } else
+        {
+            return null;
+        }
 
     }
 }
